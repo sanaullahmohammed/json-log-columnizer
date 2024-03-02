5c9540d3ceef9e96a321ab22ab3676668f8ec851
sanaullahmohammed <sanaullah.mohammad100@gmail.com>
2024-03-03 01:58:44 +0530

    complete the initial prototype

--- JsonLogColumnizer/Program.cs
+++ JsonLogColumnizer/Program.cs
@@ -1,2 +1,57 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Miner;
+using Refiner;
+using Storer;
+using System.Collections.Concurrent;
+
+namespace JsonLogColumnizer
+{
+    public class Program
+    {
+        public static bool CheckFileExists(string path)
+        {
+            return File.Exists(path);
+        }
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, from JsonLogColumnizer");
+
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Needs the path of the logfile");
+                return;
+            }
+
+            var pathOfLogFile = args[0];
+
+            if (!CheckFileExists(pathOfLogFile))
+            {
+                Console.WriteLine($"Path of logfile doesn't exist, {pathOfLogFile}");
+                return;
+            }
+
+            var readQueue = new ConcurrentQueue<object?>();
+            var writeQueue = new ConcurrentQueue<ColumnarFile?>();
+
+            var miner = new Mine(pathOfLogFile, readQueue);
+            var refiner = new Refine(readQueue, writeQueue);
+            var storer = new Store(writeQueue);
+
+            var mineThread = new Thread(miner.Begin);
+            var refineThread = new Thread(refiner.Begin);
+            var storeThread = new Thread(storer.Begin);
+
+            Console.WriteLine($"Processing Has Started at {DateTime.Now}...");
+
+            mineThread.Start();
+            refineThread.Start();
+            storeThread.Start();
+
+            mineThread.Join();
+            refineThread.Join();
+            storeThread.Join();
+
+            Console.WriteLine($"Processing Finished at {DateTime.Now}");
+        }
+    }
+}
--- Storer/Store.cs
+++ Storer/Store.cs
@@ -1,7 +1,76 @@
-﻿namespace Storer
+﻿using System.Collections.Concurrent;
+
+namespace Storer
 {
     public class Store
     {
+        private readonly ConcurrentQueue<ColumnarFile?> writeQueue;
+        private string directoryName = string.Empty;
 
+        public Store(ConcurrentQueue<ColumnarFile?> writeQueue)
+        {
+            this.writeQueue = writeQueue;
+            Console.WriteLine("Store is ready for action");
+        }
+
+        public void Begin()
+        {
+            if (!GenerateResultsDirectory() || string.IsNullOrEmpty(directoryName))
+            {
+                var errMsg = $"Couldn't generate results directory";
+                Console.WriteLine(errMsg);
+                throw new Exception(errMsg);
+            }
+
+            while (true)
+            {
+                var writeQueueSize = writeQueue.Count;
+
+                Console.WriteLine($"Size of Write-Queue is: {writeQueueSize}");
+
+                if (writeQueueSize > 0)
+                {
+                    var writeQueueEntry = writeQueue.First();
+
+                    if (writeQueueEntry == null)
+                    {
+                        Console.WriteLine($"Found NULL entry in writeQueue. Halting the storing process...");
+                        break;
+                    }
+
+                    var pathName = Path.Combine(directoryName, writeQueueEntry.FileName);
+
+                    using (StreamWriter outputFile = new StreamWriter(pathName, File.Exists(pathName)))
+                    {
+                        outputFile.WriteLine(writeQueueEntry.Content);
+                        Console.WriteLine($"Successfully created an entry in {writeQueueEntry.FileName}");
+
+                        var isWriteQueueDequeueSuccessful = writeQueue.TryDequeue(out _);
+                        Console.WriteLine($"Dequeuing of entry from writeQueue was successful? {isWriteQueueDequeueSuccessful}");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"Write-Queue is empty...");
+                }
+            }
+
+            Console.WriteLine($"Storing has ended...");
+        }
+
+        private bool GenerateResultsDirectory()
+        {
+            try
+            {
+                this.directoryName = @$"E:\C#_DEVELOPMENT\JsonLogColumnizer\results_{DateTime.Now.Ticks}";
+                Directory.CreateDirectory(directoryName);
+                return true;
+            }
+            catch (Exception err)
+            {
+                Console.WriteLine(err.Message);
+                return false;
+            }
+        }
     }
 }
